184527c5aa268983859b3b93081f965bbe4b909c
Aditya Tri Prasetyo <johndoe@example.com>
2022-10-21 20:25:37 +0700

    menambahkan popup table

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const ToDo = () =>{
     let num;
 }
+
+function popup() {
+    document.getElementById('popup').classList.remove('d-none');
+}
+
+function close_popup() {
+    document.getElementById('popup').classList.add('d-none');
+}
