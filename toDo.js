29094618e79169be39b72547c73cb909c59633f2
Emerald-Coat <rikzafatiyamulya.2019@student.uny.ac.id>
2022-10-22 08:33:52 +0700

    added todo without syling

--- toDo.js
+++ toDo.js
@@ -1,10 +1,48 @@
-const input = document.getElementsByClassName("inputBox")
-const todoList = document.querySelector(".todo-list")
-
-const addItem = () => {
-    let div = document.createElement("div");
-    let checker = document.createElement("input");
-    let span = document.createElement("span");
-    let button = document.createElement("button")
-
+const addItem = (txt) => {
+    let li = document.createElement("li");
+    const del = document.createElement("button");
+    li.innerHTML = txt;
+    list.insertBefore(li, list.childNodes[0]);
+    del.textContent = "X";
+    del.classList.add("fas", "fas-trash-alt");
+    li.appendChild(del);
+    del.addEventListener("click", (e) => {
+        li.parentNode.removeChild(li);
+        savedData = savedData.filter((e) => e !== txt);
+        localStorage.setItem("tasks", JSON.stringify(savedData))
+    });
 }
+
+let input = document.querySelector("#inputBox")
+let list = document.querySelector("#list")
+
+let savedData = JSON.parse(localStorage.getItem("tasks")) || [];
+
+savedData.forEach(addItem);
+
+
+const enterInput = () => {
+    let txt = input.value;
+    if (txt === ""){
+        alert ("Please write something!");
+    } else {
+        savedData.push(txt);
+        localStorage.setItem("tasks", JSON.stringify(savedData));
+        input.value = "";
+        addItem(txt);
+    }
+}
+
+const check = (e) => {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("checked");
+    }
+}
+
+const addListAfterKeypress = (e) => {
+    if (input.value.length > 0 && e.keyCode === 13){
+        enterInput();
+    }
+}
+
+input.addEventListener("keypress", addListAfterKeypress);
