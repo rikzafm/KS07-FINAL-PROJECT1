e2738dbe9a4805f95077ab1bcf1886cecdd74ccc
Emerald-Coat <rikzafatiyamulya.2019@student.uny.ac.id>
2022-10-22 11:10:29 +0700

    todo list : bug fixes & now save checks

--- toDo.js
+++ toDo.js
@@ -2,6 +2,7 @@ let input = document.querySelector("#inputBox")
 let list = document.querySelector("#list")
 
 let savedData = JSON.parse(localStorage.getItem("tasks")) || [];
+let checkData = JSON.parse(localStorage.getItem("checks")) || [];
 
 const addItem = (txt) => {
     let li = document.createElement("li");
@@ -15,19 +16,17 @@ const addItem = (txt) => {
         li.parentNode.removeChild(li);
         savedData = savedData.filter((e) => e !== txt);
         localStorage.setItem("tasks", JSON.stringify(savedData))
+        saveCheck();
     });
 }
 
 const enterInput = () => {
     let txt = input.value;
-    if (txt === ""){
-        alert ("Please write something!");
-    } else {
-        savedData.push(txt);
-        localStorage.setItem("tasks", JSON.stringify(savedData));
-        input.value = "";
-        addItem(txt);
-    }
+    savedData.push(txt);
+    localStorage.setItem("tasks", JSON.stringify(savedData));
+    input.value = "";
+    addItem(txt);
+    saveCheck();
 }
 
 const check = (e) => {
@@ -36,16 +35,50 @@ const check = (e) => {
     }
 }
 
+const autoCheck = () => {
+    var li = document.querySelectorAll("li");
+    checkData.map((e, i) => {
+        if(checkData[i]===true){
+            li[i].classList.toggle("checked")
+        }
+    })
+}
+
+const saveCheck = () => {
+    var li = document.querySelectorAll("li");
+    let length = li.length;
+
+    for(let i = 0; i < length; i++){
+
+        if(li[i].classList.contains("checked")){
+            checkData[i] = true;
+        } else {
+            checkData[i] = false;
+        }
+    }
+    localStorage.setItem("checks", JSON.stringify(checkData));
+}
+
+const inputEmptyAlert = () => {
+    if (input.value === ""){
+        alert ("Input is required");
+        return true;
+    }
+    return false;
+}
+
 const addListAfterKeypress = (e) => {
-    if (input.value.length > 0 && e.keyCode === 13){
+    if (e.keyCode === 13 && !inputEmptyAlert()){
         enterInput();
     }
 }
 
 const clickList = (e) => {
     check(e);
+    saveCheck();
 }
 
-savedData.forEach(addItem);
 input.addEventListener("keypress", addListAfterKeypress);
 list.addEventListener("click", clickList)
+savedData.forEach(addItem);
+autoCheck();
